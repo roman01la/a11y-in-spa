5709e2ef1190c6632f46c7cdefac1d5235d7d48f
Roman Liutikov <roman01la@romanliutikov.com>
2015-07-10 15:29:07 +0300

    ...

---
+++ material/dist/docs/js/build-config.js
@@ -0,0 +1,7 @@
+DocsApp.constant('BUILDCONFIG', {
+  "ngVersion": "1.4.1",
+  "version": "0.10.0",
+  "repository": "https://github.com/angular/material",
+  "commit": "f5959cc2e1d3a960c8df344669731c1a16c3e617",
+  "date": "2015-06-29 11:56:47 -0700"
+});
